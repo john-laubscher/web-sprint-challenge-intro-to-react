b0f4be5f7bfef8ec0cfe5f5c089c8d670147e3fa
John Laubscher <jlmemphiscommanders@gmail.com>
2021-05-14 17:04:27 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
+import Character from "./components/Character";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,35 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [characterList, setCharacterList] = useState([]);
+  const [stillLoading, setStillLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people")
+      .then((response) => {
+        console.log("2. here is future data", response);
+        setCharacterList(response.data);
+        setStillLoading(false);
+        console.log("this is character list", characterList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log("char list outside", characterList);
+  if (stillLoading) {
+    return <div>Loading</div>;
+  }
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character
+        characterList={characterList}
+        setCharacterList={setCharacterList}
+      />
     </div>
   );
 };
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
-// Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledCharacter = styled.div`
+  color: black;
+  text-shadow: 1px 1px 5px #fff;
+  font-size: 1.5em;
+  width: 22%;
+  border: 2px solid grey;
+  border-radius: 25px;
+  padding: 2px;
+  margin: 10px;
+
+  &:hover {
+    background-color: #fff;
+    font-size: 1.75em;
+  }
+`;
+
+export default function Character(props) {
+  console.log("this is props", props);
+
+  const characters = props.characterList.map((character) => {
+    console.log(character);
+    return (
+      <StyledCharacter className="individualContainer">
+        <h3>{character.name}</h3>
+      </StyledCharacter>
+    );
+  });
+  console.log("this is characters", characters);
+
+  return characters;
+}
